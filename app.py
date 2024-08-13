063823bb520bade00e4c58e51bc8bcaeb1a80445
OsmarC368 <osmargabriel10@gmail.com>
2024-08-13 16:16:34 -0400

    category Done

--- app.py
+++ app.py
@@ -4,7 +4,23 @@ from bson.objectid import ObjectId
 
 app = Flask(__name__, template_folder="./templates")
 app.config['SECRET_KEY'] = "twiceot9"
+userLoged = {}
 
+
+#=========================================================
+#  Default
+#=========================================================
+@app.route("/", methods=["GET"])
+def default():
+    return redirect(url_for('login'))
+
+@app.route("/home", methods=["GET"])
+def home():
+    return render_template('home.html.jinja', userLoged=userLoged)
+
+#=========================================================
+#  LOGIN
+#=========================================================
 @app.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "POST":
@@ -33,16 +49,58 @@ def login():
             return render_template('login.html.jinja')
         else:
             if userFound['password'] == password:
-                return render_template('home.html.jinja')
+                userLoged = userFound
+                return render_template('home.html.jinja', userLoged = userLoged)
 
     return render_template('login.html.jinja')
 
+#=========================================================
+#  CATEGORY
+#=========================================================
+
 @app.route("/category", methods=["GET", "POST"])
-def category():
+def saveCategory():
     if request.method == "POST":
-        p = 0
+        name = request.form['name']
+        description = request.form['description']
+
+        newCategory = {
+            'name': name,
+            'description': description
+        }
+
+        category.insert_one(newCategory)
+        return redirect(url_for('saveCategory'))
 
     return render_template('./category/category.html.jinja')
+
+@app.route("/categoriesList", methods=["GET"])
+def categoriesListView():
+    categoriesList = category.find()
+    return render_template('./category/categoriesList.html.jinja', categoriesList = categoriesList)
+
+@app.route("/delCategory/<id>", methods=["GET"])
+def delCategory(id):
+    oid = ObjectId(id)
+    catFound = category.find_one_and_delete({'_id' : oid})
+    return redirect(url_for('categoriesListView'))
+
+@app.route("/updateCategory/<id>", methods=["GET", "POST"])
+def modifyCategory(id):
+    oid = ObjectId(id)
+    catFound = category.find_one({'_id': oid})
+    if request.method == "POST":
+        new_category = request.form
+        categoryX = category.replace_one({'_id': oid},
+                                        {
+                                            'name': new_category['name'],
+                                            'description': new_category['description']
+                                        })
+        return redirect(url_for('categoriesListView'))
+    return render_template("./category/updateCategory.html.jinja", category=catFound)
+
+
+#=========================================================
 
 
 if __name__=='__main__':
